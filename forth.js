025db33b206bd527792bf8b7a9d4259a05a3abe4
Ramūnas Gutkovas <ramunasg@gmail.com>
2009-07-08 23:35:09 +0300

    cleanup

--- forth.js
+++ forth.js
@@ -18,7 +18,6 @@ function Forth(prgrm) {
     var r = [];
     var w = {};
     var input = null;
-    //var i = null; //stream(prgrm);
 
     this.stack        = s;
     this.return_stack = r;
@@ -101,23 +100,13 @@ function Forth(prgrm) {
                                 return [];
                              });
 
-    w['create'] = to_word(0,
-                          function() {
-                            w.bl();
-                            w.word();
-                            var word = s.pop();
-                            w[word] = [];
-                            return [];
-                          });
 
-    //var pc = 0; // program counter
     w[':'] = to_word(0, function() {
                             w.depth(); stack_depth = s.pop();
                             w.bl();
                             w.word();
                             var word = s.pop();
                             var f = function() {
-                                //pc = -1;
                                 f.pc = -1;
                                 var l = f.code.length - 1;
                                 var max_iterations = 20;
@@ -129,9 +118,6 @@ function Forth(prgrm) {
                                         break;
                                     }
                                     f.pc++;
-                                    //print("pc: " + f.pc + ", word: "
-                                          //+ find_word_by_addr(f.code[f.pc])
-                                          //+ ", cnst: " + f.code[f.pc].constant);
                                     f.code[f.pc]();
                                 }
                             };
@@ -166,21 +152,16 @@ function Forth(prgrm) {
                                     else if (im == -1) // normal
                                         current_word.code.push(s.pop()); // add to code
                                 } else {
-                                    var f = function(n) {
-                                        var cnst = function() {
-                                            s.push(n);
-                                        };
-                                        cnst.constant = n;
-                                        return cnst;
-                                    };
-                                    //var f = function(n)
-                                        //{ return function() { s.push(n); }; };
+                                    var f = function(n)
+                                        { return function() { s.push(n); }; };
                                     s.push(f(s.pop() * 1));
                                     push_word();
                                 }
                             }
                             return [];
                         });
+
+
     w['['] = to_word(0, function() {
                             compiling = false;
                             return [];
@@ -191,30 +172,14 @@ function Forth(prgrm) {
                                 return [];
                               }, true);
 
-    
-    w['see'] = to_word(0, function() {
-                            w["'"]();
-                            var word = s.pop();
-                            var dis = "";
-                            for (i in word.code) {
-                                var el = word.code[i];
-                                var a = find_word_by_addr(word.code[i]);
-                                if (a)
-                                    dis += a + " ";
-                                else if (typeof el == "object")
-                                    dis += "[" + el[0] + "] ";
-                                else
-                                    dis += "const ";
-                            }
-                            print(dis);
-                            return [];
-                          });
 
     var find_word_by_addr = function(a) {
         for (var i in w)
             if (w[i] == a) return i;
         return null;
     };
+
+
     w[';'] = to_word(0, function()  {
                             w.depth();
                             if (s.pop() != stack_depth)
@@ -226,13 +191,10 @@ function Forth(prgrm) {
     w['r>'] = to_word(0, function()  { return [r.pop()]; });
     w['r@'] = to_word(0, function()  { return [r[r.length - 1]]; });
 
-    w["'"] = to_word(0, function() { // xxx: is very similar to find
+    w["'"] = to_word(0, function() {
                             w.bl(); w.word(); var word = s.pop();
                             return [w[word]];
                         });
-
-    //w['here'] = to_word(0, function() { return []; });
-    //w[','] = to_word(1, function(a) { return []; });
 
     var push_word = to_word(1, function(a) { 
                                 current_word.code.push(a); return []; });
@@ -255,6 +217,10 @@ function Forth(prgrm) {
     w['true'] = constant_word(true);
     w['false'] = constant_word(false);
     w['not'] = to_word(1, function(a) { return [!a]; });
+    w['>'] = to_word(2, function(a,b) { return [a > b]; });
+    w['<'] = to_word(2, function(a,b) { return [a < b]; });
+    w['>='] = to_word(2, function(a,b) { return [a >= b]; });
+    w['<='] = to_word(2, function(a,b) { return [a <= b]; });
     
 
     var as = []; // address stack
@@ -338,10 +304,9 @@ function forth(p) { return new Forth(p); }
 
 
 var forth_program = <forth>
-: mamamija 9 2 3 ;
-: hello true if 4 mamamija else 2 then 1 ;
-hello .s
-: profiliavimui false if 1 else 2 then drop ;
+: rrrr dup 6 > if 1337 else 1 + .s rrrr then ;
+6 rrrr
+.s
 </forth>;
 
 
@@ -362,7 +327,7 @@ var w = f.words;
 f.run();
 
 
-prof("forth", w.profiliavimui, 1000, 3);
-function profiliavimui() { if (false) { return 1; } else {return 2;};}
-prof("js", profiliavimui , 1000, 3);
+//prof("forth", w.profiliavimui, 1000, 3);
+//function profiliavimui() { if (false) { return 1; } else {return 2;};}
+//prof("js", profiliavimui , 1000, 3);
 
